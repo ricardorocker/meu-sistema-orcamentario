c4f6c0f5646217ecb361d15ee173187b2bce8788
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-08-21 02:43:08 -0300

    Criando array de objetos para melhor adm de dados

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ let sobraSet = document.getElementById("sobraSet");
 let salario = document.getElementById("salario");
 let outrasReceitas = document.getElementById("outrasReceitas");
 
+let somaDespesas = 0;
 const despesas = 15;
 
 const contasDespesas = [];
-
-const contasDespesasMelhor = [];
 const linha = [];
 const setembro = [];
 const outubro = [];
@@ -15,18 +14,10 @@ const novembro = [];
 const dezembro = [];
 const total = [];
 
-let somaDespesas = 0;
 
 for (i = 0; i < despesas; i++) {
     let valorInput = document.getElementById(`setRow${i}`)
     contasDespesas[i] = {
-        "id": i, "descricao": `setRow${i}`, "valor": valorInput = 0
-    }
-}
-
-for (i = 0; i < despesas; i++) {
-    let valorInput = document.getElementById(`setRow${i}`)
-    contasDespesasMelhor[i] = {
         "id": i,
         "linha": linha[i] = {
             "nomeDespesa": "Aluguel",
@@ -47,83 +38,59 @@ for (i = 0; i < despesas; i++) {
                 "valor": valorInput = 0
             },            
             "total": total[i] = {
-                "descricao": `dezRow${i}`,
+                "descricao": `totalRow${i}`,
                 "valor": valorInput = 0
             },
         },
-
     }
 }
 
 onload = function () {
     // localStorage.clear();
 
-
-    console.log("Entrou no onLoad");
-
     let contasDespesasJSON = JSON.parse(localStorage.getItem("contasDespesasJSON"))
 
-    console.log("contas Despesas MELHOR", contasDespesasMelhor);
-    console.log("contas Despesas", contasDespesas);
-    console.log('contasDespesasJSON', contasDespesasJSON);
-
     for (i = 1; i < despesas; i++) {
-        contasDespesas[i].valor = contasDespesasJSON[i].valor;
+        contasDespesas[i].linha.setembro.valor = contasDespesasJSON[i].linha.setembro.valor;
 
         let elemento = document.getElementById(`setRow${i}`);
-        if (contasDespesasJSON[i].valor == 0) {
+        if (contasDespesasJSON[i].linha.setembro.valor == 0) {
         } else {
-            elemento.value = contasDespesasJSON[i].valor;
+            elemento.value = contasDespesasJSON[i].linha.setembro.valor;
         }
     }
 
     let totalDespesasSetPego = localStorage.getItem('somaDespesas');
     totalDespesasSet.innerHTML = totalDespesasSetPego;
 
-    // let salarioPego = localStorage.getItem('salario');
-    // salario.value = salarioPego;
+    let salarioPego = localStorage.getItem('salario');
+    salario.value = salarioPego;
 
-    // let outrasReceitasPego = localStorage.getItem('outrasReceitas');
-    // outrasReceitas.value = outrasReceitasPego;
+    let outrasReceitasPego = localStorage.getItem('outrasReceitas');
+    outrasReceitas.value = outrasReceitasPego;
+    sobraSet.innerHTML = (parseInt(salarioPego) + parseInt(outrasReceitasPego)) - totalDespesasSetPego;
 
-    // sobraSet.innerHTML = (parseInt(salarioPego) + parseInt(outrasReceitasPego)) - totalDespesasSetPego;
 }
 
 function updateValue(elemento) {
-
-    console.log("Soma Despesas", somaDespesas)
-
     somaDespesas = 0;
 
-    for (i = 0; i < contasDespesasMelhor.length; i++) {
+    for (i = 0; i < contasDespesas.length; i++) {
+        let monthClass = elemento.className;
 
-        if (contasDespesasMelhor[i].coluna.descricao == elemento.id) {
-            contasDespesasMelhor[i].coluna.valor = parseInt(elemento.value);
+        if (contasDespesas[i].linha[monthClass].descricao == elemento.id) {
+            contasDespesas[i].linha[monthClass].valor = parseInt(elemento.value);
         }
 
-        somaDespesas += contasDespesasMelhor[i].valor;
+        somaDespesas += contasDespesas[i].linha[monthClass].valor;
     }
 
     totalDespesasSet.innerHTML = somaDespesas;
     localStorage.setItem("somaDespesas", somaDespesas);
-    localStorage.setItem("contasDespesasJSON", JSON.stringify(contasDespesasMelhor));
+    localStorage.setItem("contasDespesasJSON", JSON.stringify(contasDespesas));
 
-    // for (i = 0; i < contasDespesas.length; i++) {
-
-    //     if (contasDespesas[i].descricao == elemento.id) {
-    //         contasDespesas[i].valor = parseInt(elemento.value);
-    //     }
-
-    //     somaDespesas += contasDespesas[i].valor;
-    // }
-
-    // totalDespesasSet.innerHTML = somaDespesas;
-    // localStorage.setItem("somaDespesas", somaDespesas);
-    // localStorage.setItem("contasDespesasJSON", JSON.stringify(contasDespesas));
-
-    // sobraSet.innerHTML = (parseInt(salario.value) + parseInt(outrasReceitas.value)) - parseInt(totalDespesasSet.textContent);
-
-    // localStorage.setItem("salario", salario.value);
-    // localStorage.setItem("outrasReceitas", outrasReceitas.value);
+    sobraSet.innerHTML = (parseInt(salario.value) + parseInt(outrasReceitas.value)) - somaDespesas;
+    localStorage.setItem("salario", salario.value);
+    localStorage.setItem("outrasReceitas", outrasReceitas.value);
 }
 
