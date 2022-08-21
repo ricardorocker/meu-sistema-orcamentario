12489e7d3b6e4366ce6ea53b8cd631441cf088e2
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-08-20 23:26:03 -0300

    Tentando usar o for pra limpar código

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ let sobraSet = document.getElementById("sobraSet");
 let salario = document.getElementById("salario");
 let outrasReceitas = document.getElementById("outrasReceitas");
 
+var despesas = 14;
+
+const contasDespesas = [];
+
+for(i = 1; i < despesas; i++){
+    
+    let valorInput = document.getElementById(`setRow${i}`)
+    
+    contasDespesas.push(parseInt(valorInput.value));    
+    
+    console.log("For", valorInput);
+
+}
+
+console.log("fora do For, contasDespesas: ", contasDespesas);
+
+
+
 let setRow1 = document.getElementById("setRow1");
 let setRow2 = document.getElementById("setRow2");
 let setRow3 = document.getElementById("setRow3");
@@ -21,47 +39,67 @@ let setRow14 = document.getElementById("setRow14");
 onload = function () {
     console.log("Entrou no onLoad");
 
-    let valorPego1 = localStorage.getItem('setRow1');
-    let valorPego2 = localStorage.getItem('setRow2');
-    let valorPego3 = localStorage.getItem('setRow3');
-    let valorPego4 = localStorage.getItem('setRow4');
-    let valorPego5 = localStorage.getItem('setRow5');
-    let valorPego6 = localStorage.getItem('setRow6');
-    let valorPego7 = localStorage.getItem('setRow7');
-    let valorPego8 = localStorage.getItem('setRow8');
-    let valorPego9 = localStorage.getItem('setRow9');
-    let valorPego10 = localStorage.getItem('setRow10');
-    let valorPego11 = localStorage.getItem('setRow11');
-    let valorPego12 = localStorage.getItem('setRow12');
-    let valorPego13 = localStorage.getItem('setRow13');
-    let valorPego14 = localStorage.getItem('setRow14');
+    for(i = 0; i < 14; i++){
+        let valorPego = localStorage.getItem(`setRow${i}`);
+    
+        let salarioPego = localStorage.getItem('salario');
+        let outrasReceitasPego = localStorage.getItem('outrasReceitas');
+    
+        let totalDespesasSetPego = localStorage.getItem('totalDespesasSet');
+    
+        totalDespesasSet.innerHTML = totalDespesasSetPego;
+    
+        salario.value = salarioPego;
+        outrasReceitas.value = outrasReceitasPego;
+    
+        sobraSet.innerHTML = (parseInt(salarioPego) + parseInt(outrasReceitasPego)) - totalDespesasSetPego;
+    
 
-    let salarioPego = localStorage.getItem('salario');
-    let outrasReceitasPego = localStorage.getItem('outrasReceitas');
 
-    let totalDespesasSetPego = localStorage.getItem('totalDespesasSet');
+        setRow1.value = valorPego;
+    }
 
-    totalDespesasSet.innerHTML = totalDespesasSetPego;
+    // let valorPego1 = localStorage.getItem('setRow1');
+    // let valorPego2 = localStorage.getItem('setRow2');
+    // let valorPego3 = localStorage.getItem('setRow3');
+    // let valorPego4 = localStorage.getItem('setRow4');
+    // let valorPego5 = localStorage.getItem('setRow5');
+    // let valorPego6 = localStorage.getItem('setRow6');
+    // let valorPego7 = localStorage.getItem('setRow7');
+    // let valorPego8 = localStorage.getItem('setRow8');
+    // let valorPego9 = localStorage.getItem('setRow9');
+    // let valorPego10 = localStorage.getItem('setRow10');
+    // let valorPego11 = localStorage.getItem('setRow11');
+    // let valorPego12 = localStorage.getItem('setRow12');
+    // let valorPego13 = localStorage.getItem('setRow13');
+    // let valorPego14 = localStorage.getItem('setRow14');
 
-    salario.value = salarioPego;
-    outrasReceitas.value = outrasReceitasPego;
+    // let salarioPego = localStorage.getItem('salario');
+    // let outrasReceitasPego = localStorage.getItem('outrasReceitas');
 
-    sobraSet.innerHTML = (parseInt(salarioPego) + parseInt(outrasReceitasPego)) - totalDespesasSetPego;
+    // let totalDespesasSetPego = localStorage.getItem('totalDespesasSet');
 
-    setRow1.value = valorPego1;
-    setRow2.value = valorPego2;
-    setRow3.value = valorPego3;
-    setRow4.value = valorPego4;
-    setRow5.value = valorPego5;
-    setRow6.value = valorPego6;
-    setRow7.value = valorPego7;
-    setRow8.value = valorPego8;
-    setRow9.value = valorPego9;
-    setRow10.value = valorPego10;
-    setRow11.value = valorPego11;
-    setRow12.value = valorPego12;
-    setRow13.value = valorPego13;
-    setRow14.value = valorPego14;
+    // totalDespesasSet.innerHTML = totalDespesasSetPego;
+
+    // salario.value = salarioPego;
+    // outrasReceitas.value = outrasReceitasPego;
+
+    // sobraSet.innerHTML = (parseInt(salarioPego) + parseInt(outrasReceitasPego)) - totalDespesasSetPego;
+
+    // setRow1.value = valorPego1;
+    // setRow2.value = valorPego2;
+    // setRow3.value = valorPego3;
+    // setRow4.value = valorPego4;
+    // setRow5.value = valorPego5;
+    // setRow6.value = valorPego6;
+    // setRow7.value = valorPego7;
+    // setRow8.value = valorPego8;
+    // setRow9.value = valorPego9;
+    // setRow10.value = valorPego10;
+    // setRow11.value = valorPego11;
+    // setRow12.value = valorPego12;
+    // setRow13.value = valorPego13;
+    // setRow14.value = valorPego14;
 }
 
 function updateValue() {
