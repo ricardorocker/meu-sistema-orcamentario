d87911ddb2cd44b1ade448a58c5fdd81f20db006
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-08-29 15:28:38 -0300

    Populando todos campos de despesas criadas

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let dezembro = [];
 let total = [];
 let mes1 = [];
 
+
 contasDespesas.push({
     "mes1": mes1 = {
         "despesas": despesas = {
@@ -107,8 +108,12 @@ contasDespesas.push({
     },
 });
 
+console.log("contasDespesas fora de escopo: ", contasDespesas);
+
 function quantidadeDespesas() {
-    return contasDespesas.length + 1;
+    if (contasDespesas) {
+        return contasDespesas.length;
+    }
 }
 
 function onRemove(elemento) {
@@ -170,6 +175,8 @@ function adicionarDespesa() {
     row.appendChild(totalRow);
     tabelaFinancas.appendChild(row);
 
+    console.log("contasDespesas dentro de adicionarDespesas: ", contasDespesas);
+
     contasDespesas.push(
         {
             "id": quantidadeDespesas(),
@@ -204,56 +211,108 @@ onload = function () {
     // for (var member in contasDespesas) delete contasDespesas[member];    
     // for (var member in contasDespesasJSON) delete contasDespesasJSON[member];
 
-    // console.log("contasDespesas ANTES", contasDespesas);
-
     let contasDespesasJSON = JSON.parse(localStorage.getItem("contasDespesasJSON"));
 
-    contasDespesas = contasDespesasJSON;
-    
+    if (contasDespesas.length > 1) {
+        contasDespesas = contasDespesasJSON;
+    }
 
-    
     console.log("salarioMes1", salarioMes1);
     console.log("contasDespesas DEPOIS", contasDespesas);
 
-    // console.log("contasDespesasJSON", contasDespesasJSON);
 
-    contasDespesas.forEach((element, i) => {
-        console.log(`PASSOU ${i + 1}ª vez`, element);
+    if (contasDespesas.length > 1 ) {
+        contasDespesas.forEach((element, i) => {
+            console.log(`PASSOU ${i + 1}ª vez`, element);
 
-        if (i == 0) {
-            console.log("despesas", element.mes1.despesas.valor)
-            despesasSet.innerHTML = element.mes1.despesas.valor;
-            despesasOut.innerHTML = element.mes2.despesas.valor;
-            despesasNov.innerHTML = element.mes3.despesas.valor;
-            despesasDez.innerHTML = element.mes4.despesas.valor;
+            if (i == 0) {
+                console.log("despesas", element.mes1.despesas.valor)
+                despesasSet.innerHTML = element.mes1.despesas.valor;
+                despesasOut.innerHTML = element.mes2.despesas.valor;
+                despesasNov.innerHTML = element.mes3.despesas.valor;
+                despesasDez.innerHTML = element.mes4.despesas.valor;
 
-            sobraMes1.innerHTML = element.mes1.sobra.valor;
-            sobraMes2.innerHTML = element.mes2.sobra.valor;
-            sobraMes3.innerHTML = element.mes3.sobra.valor;
-            sobraMes4.innerHTML = element.mes4.sobra.valor;
+                sobraMes1.innerHTML = element.mes1.sobra.valor;
+                sobraMes2.innerHTML = element.mes2.sobra.valor;
+                sobraMes3.innerHTML = element.mes3.sobra.valor;
+                sobraMes4.innerHTML = element.mes4.sobra.valor;
 
-            salarioMes1.value = element.mes1.salario.valor;
-            salarioMes2.value = element.mes2.salario.valor;
-            salarioMes3.value = element.mes3.salario.valor;
-            salarioMes4.value = element.mes4.salario.valor;
+                salarioMes1.value = element.mes1.salario.valor;
+                salarioMes2.value = element.mes2.salario.valor;
+                salarioMes3.value = element.mes3.salario.valor;
+                salarioMes4.value = element.mes4.salario.valor;
 
-            outrasReceitasMes1.value = element.mes1.outrasReceitas.valor;
-            outrasReceitasMes2.value = element.mes2.outrasReceitas.valor;
-            outrasReceitasMes3.value = element.mes3.outrasReceitas.valor;
-            outrasReceitasMes4.value = element.mes4.outrasReceitas.valor;
-        }
+                outrasReceitasMes1.value = element.mes1.outrasReceitas.valor;
+                outrasReceitasMes2.value = element.mes2.outrasReceitas.valor;
+                outrasReceitasMes3.value = element.mes3.outrasReceitas.valor;
+                outrasReceitasMes4.value = element.mes4.outrasReceitas.valor;
+            } else {
 
-    });
+                let tabelaFinancas = document.getElementById("tabelaFinancas");
 
-    // for (i = 0; i < contasDespesas.length; i++) {
-    //     contasDespesas[i].linha.setembro.valor = contasDespesasJSON[i].linha.setembro.valor;
+                console.log("Contas Despesas", quantidadeDespesas());
 
-    //     let elemento = document.getElementById(`setRow${i}`);
-    //     if (contasDespesasJSON[i].linha.setembro.valor == 0) {
-    //     } else {
-    //         elemento.value = contasDespesasJSON[i].linha.setembro.valor;
-    //     }
-    // }
+                let row = document.createElement("tr");
+                let nomeNovaDespesa = document.createElement("th");
+                nomeNovaDespesa.innerHTML = `${element.linha.nomeDespesa} <button scr="../meu-sistema-orcamentario/bin.png" class="border-0 bg-transparent ml-5" onclick="onRemove(this)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>`;
+
+                let tdSet = document.createElement("td");
+                let inputSet = document.createElement("input");
+                inputSet.setAttribute("type", "number");
+                inputSet.setAttribute("id", `setRow${i}`);
+                inputSet.setAttribute("oninput", "updateValue(this)");
+                inputSet.setAttribute("class", "setembro");
+
+                let tdOut = document.createElement("td");
+                let inputOut = document.createElement("input");
+                inputOut.setAttribute("type", "number");
+                inputOut.setAttribute("id", `outRow${i}`);
+                inputOut.setAttribute("oninput", "updateValue(this)");
+                inputOut.setAttribute("class", "outubro");
+
+                let tdNov = document.createElement("td");
+                let inputNov = document.createElement("input");
+                inputNov.setAttribute("type", "number");
+                inputNov.setAttribute("id", `novRow${i}`);
+                inputNov.setAttribute("oninput", "updateValue(this)");
+                inputNov.setAttribute("class", "novembro");
+
+                let tdDez = document.createElement("td");
+                let inputDez = document.createElement("input");
+                inputDez.setAttribute("type", "number");
+                inputDez.setAttribute("id", `dezRow${i}`);
+                inputDez.setAttribute("oninput", "updateValue(this)");
+                inputDez.setAttribute("class", "dezembro");
+
+                let totalRow = document.createElement("td");
+
+                row.appendChild(nomeNovaDespesa);
+                row.appendChild(tdSet);
+                tdSet.appendChild(inputSet);
+                row.appendChild(tdOut);
+                tdOut.appendChild(inputOut);
+                row.appendChild(tdNov);
+                tdNov.appendChild(inputNov);
+                row.appendChild(tdDez);
+                tdDez.appendChild(inputDez);
+                row.appendChild(totalRow);
+                tabelaFinancas.appendChild(row);
+
+                let inputMes1 = document.querySelector(`.setembro#setRow${i}`);
+                let inputMes2 = document.querySelector(`.outubro#outRow${i}`);
+                let inputMes3 = document.querySelector(`.novembro#novRow${i}`);
+                let inputMes4 = document.querySelector(`.dezembro#dezRow${i}`);
+
+                inputMes1.value = element.linha.setembro.valor;
+                inputMes2.value = element.linha.outubro.valor;
+                inputMes3.value = element.linha.novembro.valor;
+                inputMes4.value = element.linha.dezembro.valor;
+
+            }
+        });
+    }
 
 }
 
@@ -272,8 +331,7 @@ function updateValue(elemento) {
 
             somaDespesas += contasDespesas[i].linha[monthClass].valor;
         }
-
-
+        
         switch (monthClass) {
             case "setembro":
                 if (elemento.id == "salario") {
